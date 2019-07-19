0088c4f769e2ef2515e65a8eff8d1686617765bf
陈建军 <jianjun.chen@hand-china.com>
2019-07-19 22:14:36 +0800

    [ADD]初始化项目

---
+++ src/main/java/com/example/springboottest1/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.springboottest1.controller;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 这部分是最开始创建项目时，为了测试项目能否正常运行写的一个测试
+ *
+ * 访问http://localhost:8080/hello/springboot     可在页面看见：HelloWord
+ *
+ * */
+
+@Controller
+@RequestMapping(value = {"/hello"})
+public class HelloController {
+
+    @RequestMapping(value = {"/springboot"})
+    public String hello(HashMap<String, Object> map, Model model){
+        model.addAttribute("say","欢迎欢迎,热烈欢迎");
+        map.put("hello", "欢迎进入HTML页面");
+        return "HelloWord";
+    }
+}
